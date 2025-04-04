a35dc57cf3e430f5ae0e4039b1f761c3783dcaa7
Vinaya-17 <vinayavinaya706@gmail.com>
2025-04-04 13:10:07 +0530

    Add files via upload

---
+++ Order/src/main/java/com/atech/controller/OrderController.java
@@ -0,0 +1,5 @@
+package com.atech.controller;
+
+public class OrderController {
+
+}
---
+++ Order/src/main/java/com/atech/model/Order.java
@@ -0,0 +1,92 @@
+package com.atech.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private int orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "order_date")
+    @Temporal(TemporalType.DATE)
+    private Date orderDate;
+
+    @Column(name = "order_status", nullable = false)
+    private String orderStatus;
+
+    // Constructors
+    public Order() {}
+
+    public Order(Customer customer, Product product, Date orderDate, String orderStatus) {
+        this.customer = customer;
+        this.product = product;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+    }
+
+    // Getters and Setters
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+    
+   public void setOrderStatus(String orderStatus) {
+    	this.orderStatus=orderStatus;
+    }
+}
+
+  
